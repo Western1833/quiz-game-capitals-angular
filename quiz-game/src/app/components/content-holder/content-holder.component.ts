97dd70da26b71cb5c4df5e2046b9965ca7958a2c
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-04-16 22:18:31 +0300

    Finished styling

--- quiz-game/src/app/components/content-holder/content-holder.component.ts
+++ quiz-game/src/app/components/content-holder/content-holder.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-holder.component.css']
 })
 export class ContentHolderComponent {
+  countries: {} = {};
 
+  loadCountries() {
+    
+  }
 }
